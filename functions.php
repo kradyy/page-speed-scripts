4e01fe05e95bbd0f6bdc1829e002cb6215ba83d6
Chris Johansson <chris.johansson@mildmedia.se>
2020-08-17 16:12:56 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 // include custom jQuery
 function ao_include_custom_jquery() {
-
 	wp_deregister_script('jquery');
 	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
 }
